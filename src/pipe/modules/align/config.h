3434efd9d52ecd59462190af0d960afbd2b254af
Niklas Arlt <hahihohehe@gmail.com>
2022-08-04 11:46:50 +0200

    align: description for DT_NO_ALIGN parameter

--- src/pipe/modules/align/config.h
+++ src/pipe/modules/align/config.h
@@ -2,4 +2,6 @@
 // #define DT_BURST_HRES_FIT // 2x2
 // #define DT_BURST_QRES_FIT // 4x4
 // #define DT_ALIGN_NO_QUADRIC
-#define DT_NO_ALIGN
+
+// Uncomment the following line to disable hierarchical alignment and only use lukas kanade
+// #define DT_NO_ALIGN
