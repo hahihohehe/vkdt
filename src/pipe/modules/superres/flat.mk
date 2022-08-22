e81547aef048ff048153f166e00261989868ef8c
Niklas Arlt <hahihohehe@gmail.com>
2022-08-22 17:01:04 +0200

    superres: makefile, slider range, ...

--- src/pipe/modules/superres/flat.mk
+++ src/pipe/modules/superres/flat.mk
@@ -1,3 +1,5 @@
 MOD_C=pipe/connector.c
 pipe/modules/superres/libsuperres.so: pipe/modules/superres/config.h
 pipe/modules/superres/combine.comp.spv:pipe/modules/superres/config.h
+pipe/modules/superres/combine.comp.spv:pipe/modules/superres/common.glsl
+pipe/modules/superres/cf.comp.spv:pipe/modules/superres/common.glsl
