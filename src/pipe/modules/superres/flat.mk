4f16e7547b31bf689a60a7e579c2c976debc3517
Niklas Arlt <hahihohehe@gmail.com>
2022-08-22 12:20:11 +0200

    superres: temporarily deactivate robustness because it is not working

--- src/pipe/modules/superres/flat.mk
+++ src/pipe/modules/superres/flat.mk
@@ -1,2 +1,3 @@
 MOD_C=pipe/connector.c
-pipe/modules/align/libsuperres.so: pipe/modules/superres/config.h
+pipe/modules/superres/libsuperres.so: pipe/modules/superres/config.h
+pipe/modules/superres/combine.comp.spv:pipe/modules/superres/config.h
