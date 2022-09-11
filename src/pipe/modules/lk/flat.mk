69728ecf4f18dbd5811c677085a4bb894010d3c1
Niklas Arlt <hahihohehe@gmail.com>
2022-09-11 07:35:42 +0200

    lk: start work on separate module for lucas kanade

---
+++ src/pipe/modules/lk/flat.mk
@@ -0,0 +1,5 @@
+MOD_C=pipe/connector.c
+pipe/modules/align/half.comp.spv :pipe/modules/align/config.h
+pipe/modules/align/libalign.so   :pipe/modules/align/config.h
+pipe/modules/align/merge.comp.spv:pipe/modules/align/config.h
+pipe/modules/align/dist.comp.spv:pipe/modules/align/config.h
